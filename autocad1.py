6762c9d77d9a34a3565ab195387da305be3d17de
jiachenguoNU <90712230+jiachenguoNU@users.noreply.github.com>
2021-09-21 09:07:04 -0500

    Update autocad1.py

--- autocad1.py
+++ autocad1.py
@@ -498,6 +498,7 @@ with col3:
     #%% freq-domain features
         st.title('Features from frequency domain (CAD patients)')
         st.title('')
+        st.markdown('')
         st.header('')
         st.header('')
         st.header('')
